d883209f350c78637a8a1e3931d3caa77c5ed3e3
Ugho STEPHAN <ugho.stephan@consertotech.pro>
2017-08-23 16:34:52 +0200

    import block breaker structure

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		SceneManager.LoadScene (name);
+	}
+
+	public void LoadNextLevel(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+			// Duplicate music player, self-destructing"
+			return;
+		} 
+
+		instance = this;
+		GameObject.DontDestroyOnLoad (gameObject);
+	}
+
+}
